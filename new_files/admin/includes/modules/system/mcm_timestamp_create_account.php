d24f9eb8680271a5888fa4462e818a9307de1b67
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-10-12 22:53:44 +0200

    Update mcm_timestamp_create_account.php
    fix: class name and function display link cancel button

--- new_files/admin/includes/modules/system/mcm_timestamp_create_account.php
+++ new_files/admin/includes/modules/system/mcm_timestamp_create_account.php
@@ -5,7 +5,7 @@ defined('_VALID_XTC') or die('Direct Access to this location is not allowed.');
 use RobinTheHood\ModifiedStdModule\Classes\StdModule;
 require_once DIR_FS_DOCUMENT_ROOT . '/vendor-no-composer/autoload.php';
 
-class McmTimestampCreateAccount extends StdModule
+class mcm_timestamp_create_account extends StdModule
 {
     public function __construct()
     {
@@ -13,12 +13,17 @@ class McmTimestampCreateAccount extends StdModule
         
         $this->addKey('CHK_TIME_MIN');
     }
-       
+
+    public function process($file)
+    {
+    }
 
     public function display(): array
     {
 
-        return $this->displaySaveButton();
+		return array('text' => '<br />' . '<div align="center">' . xtc_button(BUTTON_SAVE) .
+			             xtc_button_link(BUTTON_CANCEL, xtc_href_link(FILENAME_MODULE_EXPORT, 'set=' . $_GET['set'] . '&module=' . strtolower(str_replace('MODULE_', '', $this->modulePrefix)))) . '</div>'
+			         );
     }
 
     public function install()
