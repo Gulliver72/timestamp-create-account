12e821efcbae9742a59884fcf4a8b054ccb8e102
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-13 10:59:12 +0200

    refactor: put code in a function to prevent side effects

--- new_files/includes/extra/application_top/application_top_end/mcm_timestamp_create_account.php
+++ new_files/includes/extra/application_top/application_top_end/mcm_timestamp_create_account.php
@@ -1,37 +1,40 @@
 <?php
-	/*
-	 * p3e Zeitstempel Create Account 2019-10-23
-	 * diese Datei sollte im Verzeichnis shop\includes\extra\application_top\application_top_end stehen
-	 * CHK_TIME_MIN entspricht der Zeit in Sekunden, die der Kunde zum Ausfüllen mindestens benötigen soll 
-	 * (default=2) - falls Bots durchkommen schrittweise erhöhen
-	 */
+/*
+* p3e Zeitstempel Create Account 2019-10-23
+* diese Datei sollte im Verzeichnis shop\includes\extra\application_top\application_top_end stehen
+* CHECK_TIME_MIN entspricht der Zeit in Sekunden, die der Kunde zum Ausfüllen mindestens benötigen soll 
+* (default=2) - falls Bots durchkommen schrittweise erhöhen
+*/
 
-	if (defined('MODULE_MCM_TIMESTAMP_CREATE_ACCOUNT') && MODULE_MCM_TIMESTAMP_CREATE_ACCOUNT == 'true')
-	{
-		defined('MODULE_MCM_TIMESTAMP_CREATE_ACCOUNT_CHK_TIME_MIN') || define('MODULE_MCM_TIMESTAMP_CREATE_ACCOUNT_CHK_TIME_MIN', 2);
+if (!defined('MODULE_MCM_TIMESTAMP_CREATE_ACCOUNT_STATUS') || MODULE_MCM_TIMESTAMP_CREATE_ACCOUNT_STATUS != 'true') {
+    return;
+}
 
-		if (basename($_SERVER["SCRIPT_NAME"], '.php') == 'create_account' || 
-			basename($_SERVER["SCRIPT_NAME"], '.php') == 'create_guest_account')
-		{
-        	if (!isset($_POST['action']) || 
-				(isset($_POST['action']) && $_POST['action'] != 'process'))
-			{
-				$_SESSION['timechk'] = time();   // Zeitstempel
-        	}
-        
-			if (isset($_POST['action']) && $_POST['action'] == 'process')
-			{
-            	if (isset($_SESSION['timechk']))
-				{
-                	if (time() - $_SESSION['timechk'] < MODULE_MCM_TIMESTAMP_CREATE_ACCOUNT_CHK_TIME_MIN)
-					{
-                    	// stinkt nach Bot - ab auf die Startseite
-                    	unset ($_SESSION['timechk']);
-                    	xtc_redirect(xtc_href_link(FILENAME_DEFAULT));
-                	}
-            	} else {
-                	xtc_redirect(xtc_href_link(FILENAME_DEFAULT));
-				}
-        	}
-		}
-	}
+defined('MODULE_MCM_TIMESTAMP_CREATE_ACCOUNT_CHECK_TIME_MIN') || define('MODULE_MCM_TIMESTAMP_CREATE_ACCOUNT_CHECK_TIME_MIN', 2);
+
+function mcmTimeStampCreateAccountInvoke()
+{
+    $prefix = 'mcmTimestampCreateAccount';
+
+    if (basename($_SERVER["SCRIPT_NAME"], '.php') != 'create_account' && basename($_SERVER["SCRIPT_NAME"], '.php') != 'create_guest_account') {
+        return;
+    }
+
+    $action = $_POST['action'] ?? '';
+
+    if ($action == 'process') {
+        if (isset($_SESSION[$prefix]['timeCheck'])) {
+            if (time() - $_SESSION[$prefix]['timeCheck'] < MODULE_MCM_TIMESTAMP_CREATE_ACCOUNT_CHECK_TIME_MIN) {
+                // smells like bot - back to the start page
+                unset($_SESSION[$prefix]['timeCheck']);
+                xtc_redirect(xtc_href_link(FILENAME_DEFAULT));
+            }
+        } else {
+            xtc_redirect(xtc_href_link(FILENAME_DEFAULT));
+        }
+    } else {
+        $_SESSION[$prefix]['timeCheck'] = time(); // time stamp
+    }
+}
+
+mcmTimeStampCreateAccountInvoke();
